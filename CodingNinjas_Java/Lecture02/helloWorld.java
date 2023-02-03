a150d4a53f4ed4e9ccf0dcbe71aa086adc69443d
Nehal <mdnehaladil@gmail.com>
2023-02-03 17:17:40 +0530

    avg Marks

---
+++ CodingNinjas_Java/Lecture02/helloWorld.java
@@ -0,0 +1,8 @@
+package CodingNinjas_Java;
+
+public class helloWorld {
+    public static void main(String[] args) {
+//      System.out.println("Hello World!");
+        System.out.println("Hello GitHub!");
+    }
+}
