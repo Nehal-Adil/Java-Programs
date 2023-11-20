2f1ff4e8f607ade16e517b818bdcc86b48e468be
Nehal <mdnehaladil@gmail.com>
2023-11-20 17:23:49 +0530

    first Index of Number

--- CodingNinjas_Java/Lecture14 -Recursion01/firstIndexOfNum.java
+++ CodingNinjas_Java/Lecture14 -Recursion01/firstIndexOfNum.java
@@ -39,7 +39,7 @@ public class firstIndexOfNum {
         }
 
         int[] smallArray = new int[input.length - 1];
-        for (int i = 1; i < input.length - 1; i++) {
+        for (int i = 1; i < input.length; i++) {
             smallArray[i - 1] = input[i];
         }
 
