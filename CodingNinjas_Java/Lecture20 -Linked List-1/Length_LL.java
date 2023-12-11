c260b63f49ee9be65e69a81b8084c04a163846e5
Nehal <mdnehaladil@gmail.com>
2023-12-11 12:47:56 +0530

    length of LL

--- CodingNinjas_Java/Lecture20 -Linked List-1/Length_LL.java
+++ CodingNinjas_Java/Lecture20 -Linked List-1/Length_LL.java
@@ -1,43 +1,9 @@
-/*
-                                        Length of LL
-For a given singly linked list of integers, find and return its length. Do it using an iterative method.
-Input format :
-The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
-First and the only line of each test case or query contains elements of the singly linked list separated by a single space. 
-Remember/Consider : While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
-
-Output format :
-For each test case, print the length of the linked list.
-Output for every test case will be printed in a separate line.
-
-Constraints :
-1 <= t <= 10^2
-0 <= N <= 10^5
-Time Limit: 1 sec
-
-Sample Input 1 :
-1
-3 4 5 2 6 1 9 -1
-Sample Output 1 :
-7
-
-Sample Input 2 :
-2
-10 76 39 -3 2 9 -23 9 -1
--1
-Sample Output 2 :
-8
-0
-*/
-
-// CodingNinjasNode is also included in the file, takeInput and print function are there too.
-
 package CodingNinjas_DSA;
 
 public class Length_LL {
     //Length of LinkedIn List
-    static int length(CodingNinjasNode<Integer> head) {
-        CodingNinjasNode<Integer> temp = head;
+    static int length(CN_Node<Integer> head) {
+        CN_Node<Integer> temp = head;
 
         int count = 0;
         while (temp != null) {
@@ -48,7 +14,7 @@ public class Length_LL {
     }
 
     public static void main(String[] args) {
-        CodingNinjasNode<Integer> head = CodingNinjasNode.takeInput();
+        CN_Node<Integer> head = CN_Node.takeInput();
         System.out.println(length(head));
     }
 
