ba2ba50441d6661af63b657c4bda8dde8aff8cfa
Nehal <mdnehaladil@gmail.com>
2024-01-07 09:11:38 +0530

    delete node (recursively)

---
+++ CodingNinjas_Java/Lecture21 -Linked List-2/DeleteNode_Rec.java
@@ -0,0 +1,76 @@
+/*
+Problem statement
+Given a singly linked list of integers and position 'i', delete the node present at the 'i-th' position in the linked list recursively.
+
+Note :
+Assume that the Indexing for the linked list always starts from 0.
+
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+0 <= i < M
+
+Time Limit:  2 sec
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+3
+Sample Output 1 :
+3 4 5 6 1 9
+Sample Input 2 :
+2
+30 -1
+0
+10 20 30 50 60 -1
+4
+Sample Output 2 :
+10 20 30 50
+
+    Following is the Node class already written for the Linked List
+
+    class CN_Node<T> {
+        T data;
+        CN_Node<T> next;
+
+        public CN_Node(T data) {
+            this.data = data;
+
+ */
+
+package CodingNinjas_DSA;
+
+import java.util.Scanner;
+
+public class DeleteNode_Rec {
+
+    static CN_Node<Integer> deleteRec(CN_Node<Integer> head, int pos) {
+        if (head == null) {
+            return head;
+        }
+        if (pos == 0) {
+            return head.next;
+        } else {
+            CN_Node<Integer> smallerHead = deleteRec(head.next, pos - 1);
+            head.next = smallerHead;
+            return head;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t > 0) {
+
+            CN_Node<Integer> head = CN_Node.takeInput();
+            int pos = sc.nextInt();
+            head = deleteRec(head, pos);
+            CN_Node.print_LL(head);
+
+            t = -1;
+        }
+    }
+}
