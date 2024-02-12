3f374643609ed7e850ddd399246c78648520a014
Nehal <mdnehaladil@gmail.com>
2024-02-12 14:01:52 +0530

    nodes greater than X

---
+++ CodingNinjas_Java/Lecture23 - Binary Trees I/NodesGreaterThanX.java
@@ -0,0 +1,61 @@
+/*
+                                                            Nodes Greater than X
+For a given a binary tree of integers and an integer X, find and return the total number of nodes of the given binary tree which are having data greater than X.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+1 4 2 3 -1 -1 -1
+2
+Sample Output 1:
+2
+Explanation for Sample Input 1:
+Out of the four nodes of the given binary tree, [3, 4] are the node data that are greater than X = 2.
+Sample Input 2:
+5 6 10 2 3 -1 -1 -1 -1 -1 9 -1 -1
+5
+Sample Output 2:
+3
+
+ */
+/*
+
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+package BinaryTree;
+
+public class NodesGreaterThanX {
+    static int countNodesGreaterThanX(BinaryTreeNode<Integer> root, int x) {
+        if (root == null) {
+            return 0;
+        }
+        int count = (root.data > x) ? 1 : 0;
+
+        if (root.left != null) {
+            count += countNodesGreaterThanX(root.left, x);
+        }
+        if (root.right != null) {
+            count += countNodesGreaterThanX(root.right, x);
+        }
+
+        return count;
+    }
+}
