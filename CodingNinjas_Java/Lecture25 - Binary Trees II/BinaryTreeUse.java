35dc67875df252f5d0c2bf3886d365ac1d5d9b5a
Nehal <mdnehaladil@gmail.com>
2024-02-14 21:47:07 +0530

    print tree level wise

--- CodingNinjas_Java/Lecture25 - Binary Trees II/BinaryTreeUse.java
+++ CodingNinjas_Java/Lecture25 - Binary Trees II/BinaryTreeUse.java
@@ -213,6 +213,35 @@ public class BinaryTreeUse {
         printTreeDetailed(root.right);
     }
 
+    // Print Tree Level Wise
+    static void printLevelWise(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return;
+        }
+        Queue<BinaryTreeNode<Integer>> nodesToPrint = new LinkedList<>();
+        nodesToPrint.add(root);
+
+        while (!nodesToPrint.isEmpty()) {
+            BinaryTreeNode<Integer> front = nodesToPrint.poll();
+            System.out.print(front.data + ":");
+
+            if (front.left != null) {
+                nodesToPrint.add(front.left);
+                System.out.print("L:" + front.left.data);
+            } else {
+                System.out.print("L:-1");
+            }
+
+            if (front.right != null) {
+                nodesToPrint.add(front.right);
+                System.out.print("R:" + front.right.data);
+            } else {
+                System.out.print(",R:-1");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
         // Take Tree Input with Better way
