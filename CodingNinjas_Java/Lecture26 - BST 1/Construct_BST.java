9fe83c616592a139fd2e808fb1c74d02fa2684e7
Nehal <mdnehaladil@gmail.com>
2024-02-19 21:49:21 +0530

    convert sorted array to BST

---
+++ CodingNinjas_Java/Lecture26 - BST 1/Construct_BST.java
@@ -0,0 +1,70 @@
+/*
+Problem statement
+Given a sorted integer array A of size n, which contains all unique elements. You need to construct a balanced BST from this input array. Return the root of constructed BST.
+
+Note: If array size is even, take first mid as root.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Input format:
+The first line of input contains an integer, which denotes the value of n. The following line contains n space separated integers, that denote the values of array.
+Output Format:
+The first and only line of output contains values of BST nodes, printed in pre order traversal.
+Constraints:
+Time Limit: 1 second
+
+Sample Input 1:
+7
+1 2 3 4 5 6 7
+Sample Output 1:
+4 2 1 3 6 5 7
+
+ */
+
+package BinaryTree;
+
+import java.util.Scanner;
+
+public class Construct_BST {
+
+    static BinaryTreeNode<Integer> SortedArrayToBST(int[] arr, int n) {
+        return SortedArrayToBST_helper(arr, 0, n - 1);
+
+    }
+
+    static BinaryTreeNode<Integer> SortedArrayToBST_helper(int[] arr, int si, int ei) {
+        if (si > ei) {
+            return null;
+        }
+
+        int mid = (si + ei) / 2;
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(arr[mid]);
+
+        root.left = SortedArrayToBST_helper(arr, si, mid - 1);
+        root.right = SortedArrayToBST_helper(arr, mid + 1, ei);
+
+        return root;
+    }
+
+
+    private static void preOrder(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+
+        int[] arr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+        BinaryTreeNode<Integer> ans = SortedArrayToBST(arr, len);
+        preOrder(ans);
+    }
+}
