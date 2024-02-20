744ccb65141be918abbb83d9541e3c4f925dce16
Nehal <mdnehaladil@gmail.com>
2024-02-20 16:46:16 +0530

    maximum and minimum in binary tree

---
+++ CodingNinjas_Java/Lecture25 - Binary Trees II/MaxMin_BinaryTree.java
@@ -0,0 +1,99 @@
+/*
+Problem statement
+For a given a Binary Tree of type integer, find and return the minimum and the maximum data values.
+
+Return the output as an object of Pair class, which is already created.
+
+Note:
+All the node data will be unique and hence there will always exist a minimum and maximum node data.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication
+whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The only line of output prints two integers denoting the minimum and the maximum data values respectively. A single line will separate them both.
+Constraints:
+2 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+Sample Output 1:
+1 14
+
+Sample Input 2:
+10 20 60 -1 -1 3 50 -1 -1 -1 -1
+Sample Output 2:
+3 60
+
+*/
+/*
+
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+package BinaryTree;
+
+public class MaxMin_BinaryTree {
+
+    static class Pair<T, U> {
+        T minimum;
+        U maximum;
+
+        public Pair(T minimum, U maximum) {
+            this.minimum = minimum;
+            this.maximum = maximum;
+        }
+
+    }
+
+    private static Pair<Integer, Integer> maxMin = new Pair(Integer.MAX_VALUE, Integer.MIN_VALUE);
+
+    public static Pair<Integer, Integer> getMinAndMax(BinaryTreeNode<Integer> root) {
+
+        getMinAndMax_helper(root);
+        return maxMin;
+    }
+
+    private static void getMinAndMax_helper(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return;
+        }
+
+        int rootData = root.data;
+        int maxVal = maxMin.maximum;
+        if (rootData > maxVal) {
+            maxMin.maximum = root.data;
+        }
+
+        int minVal = maxMin.minimum;
+        if (rootData < minVal) {
+            maxMin.minimum = root.data;
+        }
+
+        getMinAndMax_helper(root.left);
+        getMinAndMax_helper(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = BinaryTreeUse.takeTreeInputLevelWise();
+
+        Pair<Integer, Integer> pair = getMinAndMax(root);
+        System.out.println(pair.minimum + " " + pair.maximum);
+    }
+}
